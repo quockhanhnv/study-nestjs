6b2b794511bf0319ed0f7f4099738a5e87ccc82b
quockhanhnv <quockhanhnv0209@gmail.com>
2020-11-05 17:19:59 +0700

    test

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Post, Body, HttpException, HttpStatus, UseFilters, ForbiddenException } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+import { HttpExceptionFilter } from 'src/exception-filters/http-exception.filter';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Post()
+  @UseFilters(new HttpExceptionFilter())
+  async create(@Body() createCatDto: CreateCatDto) {
+    throw new ForbiddenException();
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+    return this.catsService.findAll();
+  }
+}
